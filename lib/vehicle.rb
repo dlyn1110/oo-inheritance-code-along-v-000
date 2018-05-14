4160c883fb6c906f410879dca1a0002f572321ef
Dani Lyn <dlyn1110@gmail.com>
2018-05-14 23:29:08 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,15 +1,18 @@
 class Vehicle
-attr_accessor :wheel_size, :wheel_number
 
-def nititialize(wheel_size, wheel_number)
-  @wheel_size = wheel_size
-  @wheel_number = wheel_number
-end
+  attr_accessor :wheel_size, :wheel_number
 
-def go
-   "vrrrrrrrooom!"
-end
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
   def fill_up_tank
     "filling up!"
+  end 
+
   end
-end
